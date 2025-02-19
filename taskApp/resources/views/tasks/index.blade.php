bccea3ed752eb490c3e9de782e0cdf8fe76c9151
ojimen412 <142631184+ojimen412@users.noreply.github.com>
2025-02-19 21:11:06 +0000

    another update

--- taskApp/resources/views/tasks/index.blade.php
+++ taskApp/resources/views/tasks/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 @section('content')
 <h1 class="text-2xl font-bold mb-4">All Tasks</h1>
--- taskApp/routes/web.php
+++ taskApp/routes/web.php
@@ -1,11 +1,5 @@
 <?php
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 Route::resource('tasks', TaskController::class);
 Route::redirect('/', '/tasks');
-
-Route::get('/', function () {
-    return view('tasks.index');
-});
