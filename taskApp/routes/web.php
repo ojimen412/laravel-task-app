e2ea523fd814ae687800f4011e7ab65a5d33ed55
ojimen412 <142631184+ojimen412@users.noreply.github.com>
2025-02-19 21:02:15 +0000

    updated day 2

--- taskApp/routes/web.php
+++ taskApp/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+Route::resource('tasks', TaskController::class);
+Route::redirect('/', '/tasks');
 
 Route::get('/', function () {
     return view('tasks.index');
