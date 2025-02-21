997350abbd20ca712bf47aa32aaf32bf08dde891
ojimen412 <142631184+ojimen412@users.noreply.github.com>
2025-02-21 20:17:27 +0000

    test

--- taskApp/app/Http/Controllers/TaskController.php
+++ taskApp/app/Http/Controllers/TaskController.php
@@ -1,18 +1,20 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-
+/**
+* Display a listing of the resource.
+*/
 public function index(Request $request)
 {
 $search = $request->input('search');
 $sort = $request->input('sort');
+
 $query = Task::query();
+
 if (!empty($search)) {
 $query->where(function($q) use ($search) {
 $q->where('task_name', 'like', '%'.$search.'%')
@@ -38,11 +40,18 @@ $tasks = $query->get();
 return view('tasks.index', compact('tasks', 'search', 'sort'));
 }
 
+
+
+/**
+* Show the form for creating a new resource.
+*/
 public function create()
 {
 return view('tasks.create');
 }
-
+/**
+* Store a newly created resource in storage.
+*/
 public function store(Request $request)
 {
 $validated = $request->validate([
@@ -57,36 +66,51 @@ $validated = $request->validate([
 Task::create($validated);
 return redirect()->route('tasks.index')->with('success', 'Task created successfully!');
 }
-
+/**
+* Display the specified resource.
+*/
 public function show(Task $task)
 {
-    return view('task.show', compact('task'));
-// }
-
+return view('tasks.show', compact('task'));
+}
+/**
+* Show the form for editing the specified resource.
+*/
 public function edit(string $id)
 {
-    $task = Task::findOrFail($id);
+$task = Task::findOrFail($id);
+return view('tasks.edit', compact('task'));
 }
+/**
+* Update the specified resource in storage.
+*/
+
+
+
 
 public function update(Request $request, string $id)
 {
-    $validated = $request->validate([
-        'task_name' => 'required|string|max:255',
-        'task_location' => 'nullable|string|max:255',
-        'time-complexity' => 'nullable|string',
-        'deadline' => 'nullable| date',
-        'priority' => 'nullable|integer|min:1|max:3',
+$validated = $request->validate([
+'task_name' => 'required|string|max:255',
+'task_location' => 'nullable|string|max:255',
+'time_complexity' => 'required|integer|min:1|max:255',
+'materials_required' => 'nullable|string',
+'deadline' => 'nullable|date',
+'priority' => 'nullable|integer|min:1|max:3',
 'category' => 'nullable|string|max:255',
 ]);
 $task = Task::findOrFail($id);
 $task->update($validated);
 return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
 }
-
+/**
+* Remove the specified resource from storage.
+*/
 public function destroy(string $id)
 {
 $task = Task::findOrFail($id);
 $task->delete();
+
 return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
-    }
 }
+};
